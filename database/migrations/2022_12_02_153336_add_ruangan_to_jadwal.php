285306b77c91b1c805cea2877985e9eee1a1a258
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-12-02 22:37:51 +0700

    FIX : ruangan migration

--- database/migrations/2022_12_02_153336_add_ruangan_to_jadwal.php
+++ database/migrations/2022_12_02_153336_add_ruangan_to_jadwal.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('jadwal', function (Blueprint $table) {
-            //
+            $table->dropColumn('ruangan');
         });
     }
 };
